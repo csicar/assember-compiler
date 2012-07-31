e37429d5c867dc1a9e9548f88b57800e835d1569
Carsten Csiky <carsten.csiky@googlemail.com>
2012-07-31 16:10:39 +0200

    working main.sj with angularjs

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
-var converte = (function(){
-	var main = function(text){
-		text.split("\n");
-		for (var i = 0; i < text.length; i++) {
-			text[i]
-		};
-	}
-	main.prototype.op = {
+function ConverterController($scope){
+	$scope.lines = [];
+	$scope.op = {
 		cpy: {
 			replace: /cpy (..),(..)/i,
 			"with": function(vars){
 				//this = main.prototype.op
-				return ["0101", op.reg(vars[0]), op.reg(vars[1])];
+				return {
+					OpBits: "0101",
+					Drain: op.reg(vars[1]), 
+					Source: op.reg(vars[2]),
+					Alu: "00000000", 
+				};
 			},
 		},
+	}
+	$scope.meta = {
 		reg: function(a){
 			return op.regs[a]
 		},
@@ -23,19 +25,24 @@ var converte = (function(){
 			dx: "11",
 		}
 	}
-	var op = main.prototype.op;
-	return main;
-})()
-
-function ConverterController($scope){
-	$scope.lines = []
+	var op = $scope.meta;
 	$scope.update = function(){
 		$scope.lines = $scope.text.split('\n');
+		console.log("asd")
 		for (var i = 0; i < $scope.lines.length; i++) {
-			$scope.convertLine($scope.lines[i])
+			$scope.lines[i] = $scope.convertLine($scope.lines[i]);
 		};
-	}
+		console.log($scope.lines)
+	};
 	$scope.convertLine = function(line){
-		
-	}  	
+		for(i in $scope.op){
+			var exp = $scope.op[i]
+			if(line.match(exp.replace)){
+				var RegExpOutp = exp.replace.exec(line);
+				line = exp.with(RegExpOutp);
+			}
+		};
+		return line;
+	}
+
 }
