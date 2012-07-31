fed0f8fbc1bb41d1be23ff73923d3a3b8ceb0f50
Carsten Csiky <carsten.csiky@googlemail.com>
2012-07-31 15:38:42 +0200

    changes

--- main.js
+++ main.js
@@ -1,9 +1,41 @@
 var converte = (function(){
 	var main = function(text){
-
+		text.split("\n");
+		for (var i = 0; i < text.length; i++) {
+			text[i]
+		};
 	}
-	main.prototype.exp = {
-		
+	main.prototype.op = {
+		cpy: {
+			replace: /cpy (..),(..)/i,
+			"with": function(vars){
+				//this = main.prototype.op
+				return ["0101", op.reg(vars[0]), op.reg(vars[1])];
+			},
+		},
+		reg: function(a){
+			return op.regs[a]
+		},
+		regs: {
+			ax: "00",
+			bx: "01",
+			cx: "10",
+			dx: "11",
+		}
 	}
+	var op = main.prototype.op;
 	return main;
 })()
+
+function ConverterController($scope){
+	$scope.lines = []
+	$scope.update = function(){
+		$scope.lines = $scope.text.split('\n');
+		for (var i = 0; i < $scope.lines.length; i++) {
+			$scope.convertLine($scope.lines[i])
+		};
+	}
+	$scope.convertLine = function(line){
+		
+	}  	
+}
