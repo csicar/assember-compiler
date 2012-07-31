b2398f4317fba72bcc6055212f962278ec07be35
Carsten Csiky <carsten.csiky@googlemail.com>
2012-07-31 15:00:20 +0200

    many things that crashed the porgram

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,44 +1,28 @@
 var converter = (function(){
+  function toHTML(input){
+		return '<span class="command">'+
+					'<span class="op-bits">'+input[0]+'</span>'+
+					'<span class="sel-bits-1">'+input[1]+'</span>'+
+					'<span class="sel-bits-2">'+input[2]+'</span>'+
+					'<span class="alu">'+input[3]+'</span>'+
+				'</span>';
+  }
   var exp = {
 		cpy: {
 			replace: /cpy (..)\,(..)/i,
-			with: '<span class="command">'+
-					'<span class="op-bits">0101</span>'+
-					'<span class="sel-bits-1">{reg: $1}</span>'+
-					'<span class="sel-bits-2">{reg: $2}</span>'+
-					'<span class="alu">0000000</span>'+
-					'<span class="gpu">0</span>'+
-				  '</span>',
+			with: ["0101","{reg: $1}","{reg: $2}", "000000000"],
 		},
 		mov: {
 			replace: /mov (..)\,(..)/i,
-			with: '<span class="command">'+
-					'<span class="op-bits">0101</span>'+
-					'<span class="sel-bits-1">{reg: $1}</span>'+
-					'<span class="sel-bits-2">{reg: $2}</span>'+
-					'<span class="alu">0000000</span>'+
-					'<span class="gpu">0</span>'+
-				  '</span>',
+			with: ["0101", "{reg: $1}", "{reg: $2}", "00000000"],
 		},
 		"no operation": {
 			replace: /no operation/i,
-			with: '<span class="command">'+
-					'<span class="op-bits">1111</span>'+
-					'<span class="sel-bits-1">00</span>'+
-					'<span class="sel-bits-2">00</span>'+
-					'<span class="alu">0000000</span>'+
-					'<span class="gpu">0</span>'+
-				  '</span>',
+			with: ["1111", "00", "00", "00000000"],
 		},
 		"alu enable": {
 			replace: /no operation/i,
-			with: '<span class="command">'+
-					'<span class="op-bits">0110</span>'+
-					'<span class="sel-bits-1">00</span>'+
-					'<span class="sel-bits-2">00</span>'+
-					'<span class="alu">0000000</span>'+
-					'<span class="gpu">0</span>'+
-				  '</span>',
+			with: ["0110", "00", "00", "00000000"],
 		},
 		"mov n": {
 			replace: /movn (..)\,([0-9a-fA-F]{1,8})/i,
@@ -50,9 +34,9 @@ var converter = (function(){
 					'<span class="gpu">0</span>'+
 				  '</span>',
 		},
-		ax: {
+		reg: {
 			replace: /\{reg: ax\}/i,
-			with: "00",
+			with: function(){return "00"},
 		},
 		bx: {
 			replace: /\{reg: bx\}/i,
@@ -71,9 +55,9 @@ var converter = (function(){
 			with: "$1",
 		},
 		hexnum: {
-			replace: /\{num: ([0-9a-fA-F]{1,8})\}/i,
-			with: function(num){
-				return parseInt(num, 16)
+			replace: /\{num: ([0-9a-fA-F]{1,2})\}/i,
+			with: function(string){
+				return parseInt(string[1], 16).toString(2);
 			},
 		},
 	};
@@ -82,14 +66,18 @@ var converter = (function(){
 			while(string.match(exp[i]["replace"])){
 				if(typeof exp[i]["with"] == 'string'){
 					string = string.replace(exp[i]["replace"], exp[i]["with"]);
-				}else{
+				}else if(typeof exp[i]["with"] == 'function'){
 					var RegExpOutp = exp[i].replace.exec(string);
-					var RegExpFuncOutp = exp[i].with(RegExpOutp[1], RegExpOutp[2], RegExpOutp);
+					var RegExpFuncOutp = exp[i].with(RegExpOutp);
 					string = string.replace(exp[i].replace, RegExpFuncOutp);
+				}else{
+					for (var n = 0; n < exp[i]["with"].length; i++) {
+						string = string.replace(exp[i]["replace"], exp[i]["with"][n]);
+					};
 				};
 			};
 		};
-		return string;	
+		return toHTML(string);	
 	};
 	function ret(input, opt){
 		var outp = "";
